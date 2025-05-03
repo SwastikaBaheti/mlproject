5bb6d2cbd69f918426f4834d1fd3df618141ad32
Swastika Baheti <swastikabaheti12@gmail.com>
2025-05-03 17:32:24 +0530

    Deployment with AWS ElasticBeanStalk

--- application.py
+++ application.py
@@ -1,7 +1,4 @@
 from flask import Flask, request, render_template
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
@@ -36,4 +33,4 @@ def predict_datapoint():
     
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
